370e39f948e03e7bcc0d5bb05a6fac1d9a062d0f
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-12-31 20:23:54 +0200

    build dashboard component

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,10 +1,38 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 
-const Dashboard = ({setAuth}) => {
+const Dashboard = ({ setAuth }) => {
+  const [name, setName] = useState('');
+
+  const getName = async () => {
+    try {
+      const response = await fetch('http://localhost:5000/dashboard/', {
+        method: 'GET',
+        headers: { token: localStorage.token }
+      });
+
+      const parseResponse = await response.json();
+      console.log('parseResponse=>', parseResponse);
+
+      setName(parseResponse.user_name)
+    } catch (err) {
+      console.error(err)
+    }
+  };
+
+  const logout = (event) => {
+    event.preventDefault();
+    localStorage.removeItem('token');
+    setAuth(false);
+  }
+
+  useEffect(() => {
+    getName();
+  });
+
   return (
     <Fragment>
-      <h1>Dashboard</h1>
-      <button onClick={() => setAuth(false)}>Logout</button>
+      <h1>Dashboard {name}</h1>
+      <button className='btn btn-primary' onClick={event => logout(event)}>Logout</button>
     </Fragment>
   );
 };
