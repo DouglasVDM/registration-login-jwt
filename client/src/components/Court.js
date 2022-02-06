1d1cb297e90339c100d0f0e4d2b9a2b312699eb8
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2022-02-06 11:48:29 +0200

    display bookings by court id

--- client/src/components/Court.js
+++ client/src/components/Court.js
@@ -5,10 +5,10 @@ const Court = ({ courtId }) => {
   const [bookings, setBookings] = useState([]);
 
 
-  // GET ALL Bookings
+  // GET ALL BOOKINGS BY COURT ID
   const getBookings = async () => {
     try {
-      const response = await fetch(`http://localhost:5000/dashboard/booking/${courtId}`, {
+      const response = await fetch(`http://localhost:5000/dashboard/bookings/${courtId}`, {
         method: 'GET'
       });
 
@@ -28,13 +28,15 @@ const Court = ({ courtId }) => {
 
   return (
     <div>
-      <h1>Court {courtId}</h1>
+      <h1>Court {courtId} Bookings</h1>
       {bookings.map(({ booking_date, time_in, time_out, booking_id }) => {
         return (
           <div>
-            <h6 key={booking_id}>Booked on {booking_date} from { time_in} to {time_out }</h6>
-        </div>
-            )
+            <h6 style={{ background: 'skyblue' }} key={booking_id}>
+              Booked: {booking_date} <br />{time_in} to {time_out}
+            </h6>
+          </div>
+        )
       })}
     </div>
   );
