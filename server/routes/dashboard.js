30209fee35c2d7f6f03b939e7dc79ddb652eb14b
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2022-02-06 11:51:42 +0200

    Create and read of booking CRUD

--- server/routes/dashboard.js
+++ server/routes/dashboard.js
@@ -18,30 +18,97 @@ router.get('/', authorization, async (req, res) => {
 });
 
 
-// GET ALL BOOKINGS FOR SPECIFIC COURT
-router.get('/booking/:id', async (req, res) => {
+// GET ALL USERS
+router.get('/users', async (req, res) => {
+  try {
+
+    const users = await pool.query(`SELECT * FROM users AS u`);
+
+    res.json(users.rows);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json('Server Error - All users');
+  }
+});
+
+
+// GET ALL BOOKINGS
+router.get('/bookings', async (req, res) => {
+  try {
+
+    const bookings = await pool.query(`SELECT * FROM bookings AS b`);
+
+    res.json(bookings.rows);
+
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server GET AllBookings Error');
+  }
+});
+
+
+// GET BOOKINGS BY COURT ID
+router.get('/bookings/:courtId', async (req, res) => {
   try {
     // 1. DESTRUCTURE req.body
-    const {id} = req.params;
+    const { courtId } = req.params;
 
     // 2. CHECK IF BOOKING EXIST (IF BOOKING EXIST THEN THROW ERROR)
-    const booking = await pool.query(`SELECT booking_date, time_in, time_out FROM bookings AS b INNER JOIN courts AS c ON b.court_id = c.court_id WHERE b.court_id = ${id}`);
+    const booking = await pool.query(`SELECT booking_date, time_in, time_out FROM bookings AS b INNER JOIN courts AS c ON b.court_id = c.court_id WHERE b.court_id = ${courtId}`);
 
     res.json(booking.rows);
 
   } catch (err) {
     console.error(err.message);
-    res.status(500).send('Server Add BookingID Error');
+    res.status(500).send('Server GET BookingByCourtId Error');
   }
 });
 
-// GET ALL BOOKINGS FOR SPECIFIC COURT
-router.get('/booking', async (req, res) => {
+
+// GET BOOKINGS BY USER ID
+router.get('/bookings/:userId', async (req, res) => {
   try {
     // 1. DESTRUCTURE req.body
-    const {court_id} = req.params;
+    const { userId } = req.params;
 
-   
+    // 2. CHECK IF BOOKING EXIST (IF BOOKING EXIST THEN THROW ERROR)
+    const booking = await pool.query(`SELECT booking_date, time_in, time_out FROM bookings AS b INNER JOIN users AS u ON b.user_id = u.user_id WHERE b.user_id = ${userId}`);
+
+    res.json(booking.rows);
+
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server GET BookingByPlayerId Error');
+  }
+});
+
+
+// ALL BOOKINGS FOR COURT 1 BY DATE ASCENDING
+router.get('/date', async (req, res) => {
+  try {
+    const dateAsc = await pool.query('SELECT booking_date FROM bookings AS b INNER JOIN courts AS c ON b.court_id = c.court_id INNER JOIN users AS u ON b.user_id = u.user_id WHERE c.court_id = 1 ORDER BY booking_date ASC');
+
+    res.json(dateAsc.rows);
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).json('Server error - Dashboard/dateAsc')
+  }
+});
+
+
+// ALL BOOKINGS FOR COURT 1 BY TIME ASCENDING
+router.get('/time-in', async (req, res) => {
+  try {
+    const timeIn = await pool.query('SELECT time_in FROM bookings AS b INNER JOIN courts AS c ON b.court_id = c.court_id INNER JOIN users AS u ON b.user_id = u.user_id WHERE c.court_id = 1 ORDER BY time_in ASC');
+
+    res.json(timeIn.rows);
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).json('Server error - Dashboard/timeIn')
+  }
+});
+
+
 // ADD NEW BOOKING
 router.post('/booking', async (req, res) => {
   try {
@@ -73,6 +140,7 @@ router.post('/booking', async (req, res) => {
   }
 });
 
+
 // ALL COURTS
 router.get('/courts', async (req, res) => {
   try {
@@ -82,31 +150,6 @@ router.get('/courts', async (req, res) => {
   } catch (err) {
     console.error(err.message)
     res.status(500).json('Server error - Dashboard/court')
-  }
-});
-
-// ALL BOOKINGS FOR COURT 1 BY DATE DESCENDING
-router.get('/date', async (req, res) => {
-  try {
-    const dateAsc = await pool.query('SELECT booking_date FROM bookings AS b INNER JOIN courts AS c ON b.court_id = c.court_id INNER JOIN users AS u ON b.user_id = u.user_id WHERE c.court_id = 1 ORDER BY booking_date ASC');
-
-    res.json(dateAsc.rows);
-  } catch (err) {
-    console.error(err.message)
-    res.status(500).json('Server error - Dashboard/dateAsc')
-  }
-});
-
-
-// ALL BOOKINGS FOR COURT 1 BY TIME DESCENDING
-router.get('/time-in', async (req, res) => {
-  try {
-    const timeIn = await pool.query('SELECT time_in FROM bookings AS b INNER JOIN courts AS c ON b.court_id = c.court_id INNER JOIN users AS u ON b.user_id = u.user_id WHERE c.court_id = 1 ORDER BY time_in ASC');
-
-    res.json(timeIn.rows);
-  } catch (err) {
-    console.error(err.message)
-    res.status(500).json('Server error - Dashboard/timeIn')
   }
 });
 
