0993723fe59b0c83f7ca8502981563b5af159af2
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2022-02-07 20:54:02 +0200

    enpoint routes

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const cors = require('cors');
-
 const app = express();
-
 const PORT = process.env.PORT || 5000;
+const db = require('./queries');
+
+
 
 // MIDDLEWARE
 app.use(express.json()); // ACCESS TO req,body
@@ -16,6 +17,13 @@ app.use('/auth', require('./routes/jwtAuth'));
 
 // DASHBOARD ROUTE
 app.use('/dashboard', require('./routes/dashboard'));
+
+// USERS
+app.get('/users', db.getUsers);
+app.get('/user', db.getUserByEmail);
+
+// BOOKINGS
+app.put('/booking/:bookingId', db.updateBooking);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`)
