e2243937e340981be2b3a42dcb561d2dcf8d8a9d
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2022-02-15 20:31:27 +0200

    fix branch conflicts

---
+++ client/src/components/Booking.js
@@ -0,0 +1,75 @@
+import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { toast } from 'react-toastify';
+
+const Booking = ({userEmail, courtId, courts, getCourtId }) => {
+  const [inputs, setInputs] = useState({
+    booking_date: '',
+    time_in: '',
+    time_out: ''
+  });
+  const user_email = userEmail;
+  const court_name = courtId;
+
+  const { booking_date, time_in, time_out } = inputs;
+
+  const onChange = (event) => {
+    setInputs({ ...inputs, [event.target.name]: event.target.value });
+  };
+
+  const onSubmitForm = async (event) => {
+    event.preventDefault()
+
+    try {
+      const body = {user_email, court_name, booking_date, time_in, time_out };
+      const response = await fetch('http://localhost:5000/booking', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(body)
+      });
+
+      const parseResponse = await response.json();
+      console.log('parseResponse_booking', parseResponse);
+
+    } catch (err) {
+      console.error(err.message);
+    }
+    setInputs({ ...inputs, [event.target.name]: event.target.value });
+  };
+
+  return (
+    <Fragment>
+      <h1 className='text-center my-5'>Booking</h1>
+      <form onSubmit={onSubmitForm}>
+        <input
+          type="date"
+          name='booking_date'
+          placeholder='date'
+          className='form-control my-3'
+          value={booking_date}
+          onChange={event => onChange(event)}
+        />
+        <input
+          type="time"
+          name='time_in'
+          placeholder='start time'
+          className='form-control my-3'
+          value={time_in}
+          onChange={event => onChange(event)}
+        />
+        <input
+          type="time"
+          name='time_out'
+          placeholder='end time'
+          className='form-control my-3'
+          value={time_out}
+          onChange={event => onChange(event)}
+        />
+        <button className="btn btn-success btn-block">Submit</button>
+        {/* <Link to='/dashboard'>Login</Link> */}
+      </form>
+    </Fragment>
+  );
+};
+
+export default Booking;
--- server/middleware/validInfo.js
+++ server/middleware/validInfo.js
@@ -1,22 +1,43 @@
 module.exports = function (req, res, next) {
-  const { name, email, password } = req.body;
+  const {
+    user_name,
+    user_email,
+    user_password,
+    admin_name,
+    admin_email,
+    admin_password,
+} = req.body;
 
 
   function validEmail(userEmail) {
     return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(userEmail);
   }
 
-  if (req.path === '/register') {
-    if (![email, name, password].every(Boolean)) {
+  if (req.path === '/user/register') {
+    if (![user_name, user_email, user_password].every(Boolean)) {
       return res.status(401).json('Missing Credentials');
-    } else if (!validEmail(email)) {
+    } else if (!validEmail(user_email)) {
       return res.status(401).json('Invalid Email');
     }
-  } else if (req.path === '/login') {
-    if (![email, password].every(Boolean)) {
+  } else if (req.path === '/user/login') {
+    if (![user_email, user_password].every(Boolean)) {
       return res.status(401).json('Missing Credentials');
-    } else if (!validEmail(email)) {
+    } else if (!validEmail(user_email)) {
       return res.status(401).json('Invalid Email')
+    }
+  }
+
+  if (req.path === "/admin/register") {
+    if (![admin_email, admin_name, admin_password].every(Boolean)) {
+      return res.status(401).json("Missing Credentials");
+    } else if (!validEmail(admin_email)) {
+      return res.status(401).json("Invalid Email");
+    }
+  } else if (req.path === "/admin/login") {
+    if (![admin_email, admin_password].every(Boolean)) {
+      return res.status(401).json("Missing Credentials");
+    } else if (!validEmail(admin_email)) {
+      return res.status(401).json("Invalid Email");
     }
   }
   next();
