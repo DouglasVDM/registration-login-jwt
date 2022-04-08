47c9e87cc45dbe18cba60cb4228fb85cfc59879d
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2022-04-08 23:11:04 +0200

    revert endpoints to start again

--- server/index.js
+++ server/index.js
@@ -2,29 +2,20 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 5000;
-const db = require('./queries');
-
 
 
 // MIDDLEWARE
 app.use(express.json()); // ACCESS TO req,body
 app.use(cors());
 
-// ROUTES
 
+// ROUTES
 // REGISTER AND LOGIN ROUTES
 app.use('/auth', require('./routes/jwtAuth'));
 
 // DASHBOARD ROUTE
 app.use('/dashboard', require('./routes/dashboard'));
 
-// USERS
-app.get('/users', db.getUsers);
-app.get('/user', db.getUserByEmail);
-
-// BOOKINGS
-app.post('/booking', db.createBooking);
-app.put('/booking/:bookingId', db.updateBooking);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`)
