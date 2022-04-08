8f1b950573c38d53a0daea4ebc7955ef6bb247c7
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2022-04-08 23:08:47 +0200

    make code more readable

--- server/middleware/authorization.js
+++ server/middleware/authorization.js
@@ -10,8 +10,11 @@ module.exports = async (req, res, next) => {
     }
 
     const payload = jwt.verify(jwtToken, process.env.jwtSecret);
+
     req.user = payload.user;
+
     next();
+    
   } catch (err) {
     console.error(err.message);
     return res.status(403).json('Not Authorize');
