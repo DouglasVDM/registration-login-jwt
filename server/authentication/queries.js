db3ca7dc5a5d37455d9958d48590bf62c0607297
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2022-04-09 14:57:17 +0200

    revert queries to start again

--- server/authentication/queries.js
+++ server/authentication/queries.js
@@ -1,18 +1,71 @@
-
-// USER QUERIES
-const checkUserEmail = "SELECT * FROM users WHERE user_email = $1";
-
-const registerUser = "INSERT INTO users (user_name, user_email, user_password) VALUES ($1, $2, $3) RETURNING *";
-
-const getUserById = "SELECT user_name FROM users WHERE user_id = $1";
+const pool = require('./db');
+const authorization = require('./middleware/authorization');
 
 
-// Admin queries
-const checkAdminEmail = "SELECT * FROM admin WHERE admin_email = $1";
+// GET ALL USERS
+const getUsers = async (req, res) => {
+  try {
 
-const registerAdmin = "INSERT INTO admin (admin_name, admin_email, admin_password) VALUES ($1, $2, $3) RETURNING *";
+    const users = await pool.query(`SELECT * FROM users AS u ORDER BY user_name ASC`);
 
-const getAdminById = "SELECT admin_name FROM admin WHERE admin_id = $1";
+    res.json(users.rows);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json('Server Error - getUsers');
+  }
+};
+
+
+// GET USER BY EMAIL
+const getUserByEmail = async (req, res) => {
+  try {
+    const { userEmail } = req.query;
+
+    const user = await pool.query(`SELECT user_name, user_email FROM users AS u WHERE user_email = $1`, [userEmail]);
+
+    res.json(user.rows);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json('Server Error - getUserEmail');
+  }
+};
+
+
+// ADD NEW BOOKING
+router.post('/bookings', async (req, res) => {
+  try {
+    // 1. DESTRUCTURE req.body
+    const {
+      user_id,
+      court_id,
+      booking_date,
+      time_in,
+      time_out } = req.body;
+
+    // 2. CHECK IF BOOKING EXIST (IF BOOKING EXIST THEN THROW ERROR)
+    // const booking = await pool.query('SELECT * FROM bookings WHERE (court_id, booking_date, time_in, time_out) = ($1, $2, $3, $4)', [court_id, booking_date, time_in, time_out]);
+
+    // if (booking.rows.length !== 0) {
+    //   return res.json({ status: 'fail', message: 'Booking already exist' })
+    // };
+
+    // 3 CHECK IF USER HAS BOOKED A COURT FOR THE WEEK
+    // const user = await pool.getUserById;
+
+    // if (user.rows.length !== 0) {
+    //   return res.json({ status: 'fail', message: 'User only allowed 1 booking per week' })
+    // };
+
+    // 4, ENTER THE NEW BOOKING INSIDE OUR DATABASE
+    const newBooking = await pool.query('INSERT INTO bookings (user_id, court_id, booking_date, time_in, time_out) VALUES ($1, $2, $3, $4, $5) RETURNING *', [user_id, court_id, booking_date, time_in, time_out]);
+
+    res.json({ status: "success", message: 'Booking created', bookings: newBooking });
+
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Add Booking Error');
+  }
+});
 
 
 module.exports = {
